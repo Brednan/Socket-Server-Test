7efafe6bdf2681b0ce8eda322f867289029426e7
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-04-19 13:14:26 -0700

    made small changes

--- server.py
+++ server.py
@@ -5,16 +5,30 @@ PORT = 65432
 
 print('Starting server...')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind((HOST, PORT))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
 
-    server.listen()
-    conn, addr = server.accept()
+server.listen()
+conn, addr = server.accept()
 
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+with conn:
+    print(f"Connected by {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+#     server.bind((HOST, PORT))
+
+#     server.listen()
+#     conn, addr = server.accept()
+
+#     with conn:
+#         print(f"Connected by {addr}")
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#             conn.sendall(data)
