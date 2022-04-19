44d887b60dc4c9be9ac6b3fff55c8a0472d49fb1
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-04-19 13:54:28 -0700

    debugging client

--- client.py
+++ client.py
@@ -6,5 +6,3 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((HOST, PORT))
     client.sendall(b'Hello Laptop')
-    data = client.recv(1024)
-    print(str(data.decode('utf-8')))
