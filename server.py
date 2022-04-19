ecc1173aba51fdfd1a8efa7f6775c50a84579d5d
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-04-18 19:27:11 -0700

    Created script for the server

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = "192.168.1.71"
+PORT = 65432
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+conn, addr = server.accept()
+
+with conn:
+    print(f"Connected by {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
